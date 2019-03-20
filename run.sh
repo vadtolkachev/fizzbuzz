672ccae6a03cbac988a5bcecf80ee580e0af2691
vadym <vadtolkachev@gmail.com>
2019-03-21 00:41:36 +0300

    added script for test coverage

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 ./test.x
+export CODECOV_TOKEN="52216a41-594e-427c-a1dd-24f270b026a2"
 lcov --no-external --capture --directory . --output-file cov.info
 genhtml cov.info --output-directory covinfo
+bash <(curl -s https://codecov.io/bash) -f cov.info
